9dd5a949f91afec6c1e1f9b67fa87d47d8e5f004
pkjq11 <ks_strong@163.com>
2025-01-21 17:07:51 +0800

    Update Video.vue

--- src/components/sections/Video.vue
+++ src/components/sections/Video.vue
@@ -16,7 +16,7 @@
         <!-- local -->
         <el-container class="video-container">
           <video controls muted preload playsinline>
-            <source src="/video/video.mp4" type="video/mp4">
+            <source src="/video/demo.mp4" type="video/mp4">
           </video>
         </el-container>
         
