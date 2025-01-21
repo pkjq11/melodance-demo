e696fa0393bae3a3680f0843d9854c22fdc9b4e1
pkjq11 <ks_strong@163.com>
2025-01-21 17:25:41 +0800

    Update Video.vue

--- src/components/sections/Video.vue
+++ src/components/sections/Video.vue
@@ -12,25 +12,33 @@
     <!-- 每个网站的视频的iframe可能不一致，最好在这里手动调整 -->
     <el-row justify="center">
       <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="10" >
-
-        <!-- local -->
-        <el-container class="video-container">
-          <video controls muted preload playsinline>
-            <source src="/video/demo.mp4" type="video/mp4">
-          </video>
-        </el-container>
-        
-        <!-- bilibili -->
-        <el-container class="video-container">
-          <iframe src="//www.bilibili.com/blackboard/html5mobileplayer.html?bvid=BV1zw68YsEP9" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
-        </el-container>
-
-        <!-- youtube -->
-        <el-container class="video-container">
-          <iframe src="https://www.youtube.com/embed/wjZofJX0v4M?si=BFvRyc3n3fFV_f1G" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </el-container>
+        <el-row>
+          <el-col :span="12">
+            <!-- local -->
+            <el-container class="video-container">
+              <video controls muted preload playsinline>
+                <source src="/video/demo.mp4" type="video/mp4">
+              </video>
+            </el-container>
+          </el-col>
+          <el-col :span="12">
+            <!-- bilibili -->
+            <el-container class="video-container">
+              <iframe src="//www.bilibili.com/blackboard/html5mobileplayer.html?bvid=BV1zw68YsEP9" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+            </el-container>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+            <!-- youtube -->
+            <el-container class="video-container">
+              <iframe src="https://www.youtube.com/embed/wjZofJX0v4M?si=BFvRyc3n3fFV_f1G" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </el-container>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
+    
   </div>
 </template>
 
