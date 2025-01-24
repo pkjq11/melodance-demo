936c28efd1480160cff2ad58fddcce2642834bb0
Shayebh <32875041+Shayebh@users.noreply.github.com>
2025-01-24 16:50:16 +0800

    Update Video.vue

--- src/components/sections/Video.vue
+++ src/components/sections/Video.vue
@@ -48,7 +48,7 @@
         </el-row>
       </el-col>
     </el-row>
-    
+    <div v-space="20"></div>
     <el-row justify="center">
       <h1 class="section-title">Explainer Video in the Wild Music</h1>
     </el-row>
@@ -108,7 +108,32 @@
         </el-row>
       </el-col>
     </el-row>
-    
+    <div v-space="20"></div>
+    <el-row justify="center">
+      <h1 class="section-title">Explainer Video for Dance consistency</h1>
+    </el-row>
+    <el-row justify="center">
+      <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="10" >
+        <el-row>
+          <el-col :span="12">
+            <!-- local -->
+            <el-container class="video-container">
+              <video controls muted preload playsinline>
+                <source src="/video/fortnight_compare.mp4" type="video/mp4">
+              </video>
+            </el-container>
+          </el-col>
+          <el-col :span="12">
+            <!-- local -->
+            <el-container class="video-container">
+              <video controls muted preload playsinline>
+                <source src="/video/Havana_compare.mp4" type="video/mp4">
+              </video>
+            </el-container>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
